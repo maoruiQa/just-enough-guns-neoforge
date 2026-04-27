2475d43f3b5c000b3567b321ccd73a0cba908f75
maoruiq <maoruiq>
2026-04-27 22:14:58 +0200

    Hide muzzle flash while ADS

--- src/client/java/ttv/migami/jeg/client/FabricClientBootstrap.java
+++ src/client/java/ttv/migami/jeg/client/FabricClientBootstrap.java
@@ -548,6 +548,10 @@ public final class FabricClientBootstrap {
     }
 
     public static void showMuzzleFlash(int entityId, float random) {
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft.player != null && minecraft.player.getId() == entityId && AimingHandler.get().isAiming()) {
+            return;
+        }
         MUZZLE_FLASHES.put(entityId, new MuzzleFlashState(2, random));
     }
 
