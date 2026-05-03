a293dc0753a7d0b5fbcb308d3d06433c70f93ae6
maoruiq <maoruiq>
2026-05-03 13:28:29 +0200

    Revert "fix: use holy shotgun first-person transform for ADS"
    This reverts commit 695e5bdfdf3e2dd30222bcdaf76d917ccbc5991e.

--- src/main/java/ttv/migami/jeg/client/render/gun/AnimatedGunRenderer.java
+++ src/main/java/ttv/migami/jeg/client/render/gun/AnimatedGunRenderer.java
@@ -237,10 +237,7 @@ public final class AnimatedGunRenderer extends GeoItemRenderer<AnimatedGunItem> 
 
     private static ItemTransform staticItemFirstPersonTransform(ResourceLocation itemId, ItemDisplayContext displayContext) {
         Minecraft mc = Minecraft.getInstance();
-        ResourceLocation modelPath = itemId.equals(Reference.id("holy_shotgun"))
-                ? Reference.id("item/first_person/holy_shotgun")
-                : Reference.id("item/" + itemId.getPath());
-        ModelResourceLocation modelId = new ModelResourceLocation(modelPath, "standalone");
+        ModelResourceLocation modelId = new ModelResourceLocation(Reference.id("item/" + itemId.getPath()), "standalone");
         BakedModel model = mc.getModelManager().getModel(modelId);
         if (model == mc.getModelManager().getMissingModel()) {
             return new ItemTransform(
