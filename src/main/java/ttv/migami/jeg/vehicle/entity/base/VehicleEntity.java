eb1c9e4dc5ca9e29a7ffa566e4f87fae271f88a5
maoruiq <maoruiq>
2026-05-17 12:43:58 +0200

    Play vehicle metal hit on all armor hits

--- src/main/java/ttv/migami/jeg/vehicle/entity/base/VehicleEntity.java
+++ src/main/java/ttv/migami/jeg/vehicle/entity/base/VehicleEntity.java
@@ -3725,10 +3725,10 @@ public class VehicleEntity extends Entity implements MenuProvider, GeoEntity {
         OBBInfo.Part hitPart = this.estimateHitPart(source);
         ArmorHit armorHit = this.applyVehicleArmor(source, amount, hitPart);
         float finalDamage = this.vehicleData().defaults().damageModifier().apply(armorHit.finalDamage());
+        this.playVehicleDamageSound(armorHit.penetrated());
         if (finalDamage <= 1.0F) {
             return false;
         }
-        this.playVehicleDamageSound(armorHit.penetrated());
         this.applyPartDamage(hitPart, finalDamage);
         this.applyPassengerLeakDamage(source, finalDamage, hitPart, armorHit.penetrated());
         this.repairCooldown = this.vehicleData().defaults().autoRepairCooldownTicks();
