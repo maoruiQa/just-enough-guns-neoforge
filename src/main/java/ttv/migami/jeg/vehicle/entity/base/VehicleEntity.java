6e5efc7029020f2eeb97e3c60c5f94eb99ad1cfb
maoruiq <maoruiq>
2026-05-24 22:39:19 +0200

    Fix vehicle driver camera positions

--- src/main/java/ttv/migami/jeg/vehicle/entity/base/VehicleEntity.java
+++ src/main/java/ttv/migami/jeg/vehicle/entity/base/VehicleEntity.java
@@ -4101,9 +4101,6 @@ public class VehicleEntity extends Entity implements MenuProvider, ExtendedMenuP
     }
 
     private Vec3 seatOffset(SeatInfo seat, double eyeHeight, float partialTick) {
-        if (this.usesArticulatedSeatTransform(seat)) {
-            return this.articulatedTurretOffset(seat.x(), seat.y() + eyeHeight, seat.z(), partialTick);
-        }
         if (this.usesVehiclePoseTransform()) {
             return this.rotateLocalOffsetWithPose(seat.x(), seat.y() + eyeHeight, seat.z(), partialTick);
         }
