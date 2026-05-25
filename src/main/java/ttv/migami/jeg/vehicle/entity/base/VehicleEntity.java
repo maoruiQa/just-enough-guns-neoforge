586b17022b298961f6b440173a1b8adb7f014513
maoruiq <maoruiq>
2026-05-25 13:13:25 +0200

    Make helicopter vehicles ignore fall impact damage

--- src/main/java/ttv/migami/jeg/vehicle/entity/base/VehicleEntity.java
+++ src/main/java/ttv/migami/jeg/vehicle/entity/base/VehicleEntity.java
@@ -1179,7 +1179,9 @@ public class VehicleEntity extends Entity implements MenuProvider, ExtendedMenuP
                 || this.ramDamageCooldown > 0) {
             return;
         }
-        boolean landVehicle = this.vehicleData().defaults().vehicleType() == VehicleType.LAND;
+        VehicleType vehicleType = this.vehicleData().defaults().vehicleType();
+        boolean landVehicle = vehicleType == VehicleType.LAND;
+        boolean helicopterVehicle = vehicleType == VehicleType.HELICOPTER;
         boolean supportedAfterMove = this.onGround() || this.verticalCollisionBelow;
         int fallAirborneTicks = landVehicle ? LAND_VEHICLE_FALL_DAMAGE_AIRBORNE_TICKS : 8;
         boolean landedAfterFall = !wasVerticallySupported
@@ -1198,7 +1200,9 @@ public class VehicleEntity extends Entity implements MenuProvider, ExtendedMenuP
             horizontalImpactSpeed = 0.0D;
         }
         double verticalImpactSpeed = this.verticalCollision && !wasVerticallySupported ? Math.abs(requestedMovement.y() - actualMovement.y()) : 0.0D;
-        if (landedAfterFall) {
+        if (helicopterVehicle) {
+            verticalImpactSpeed = 0.0D;
+        } else if (landedAfterFall) {
             double fallDurationBonus = Math.max(0, unsupportedTicksBeforeMove - fallAirborneTicks) * 0.015D;
             verticalImpactSpeed = Math.max(verticalImpactSpeed, Math.abs(requestedMovement.y()) + fallDurationBonus);
         }
