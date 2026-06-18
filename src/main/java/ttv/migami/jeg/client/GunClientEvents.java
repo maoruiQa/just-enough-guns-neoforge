599988a6c236b53d1207688f7ca3335ba0907ff1
maoruiq <maoruiq>
2026-06-18 15:10:32 +0200

    Add gun inspect animation keybind

--- src/main/java/ttv/migami/jeg/client/GunClientEvents.java
+++ src/main/java/ttv/migami/jeg/client/GunClientEvents.java
@@ -463,6 +463,14 @@ public final class GunClientEvents {
                 NetworkHandler.sendMelee();
             }
         }
+        if (!(player.getVehicle() instanceof VehicleEntity) && KeyBindings.INSPECT.consumeClick()) {
+            if (heldMain.getItem() instanceof GunItem) {
+                if (heldMain.getItem() instanceof AnimatedGunItem) {
+                    AnimatedGunItem.triggerClientInspect(minecraft.player);
+                }
+                NetworkHandler.sendInspect();
+            }
+        }
 
     }
 
