28a07c99aa8636cf1bf9fd9cc6dd8df7de8795ca
maoruiq <maoruiq>
2026-07-02 18:46:35 +0200

    Fix magazine loader orientation and drops

--- src/main/java/ttv/migami/jeg/block/MagazineLoaderBlock.java
+++ src/main/java/ttv/migami/jeg/block/MagazineLoaderBlock.java
@@ -2,19 +2,26 @@ package ttv.migami.jeg.block;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -23,9 +30,11 @@ import ttv.migami.jeg.init.ModBlockEntities;
 
 public final class MagazineLoaderBlock extends BaseEntityBlock {
     public static final MapCodec<MagazineLoaderBlock> CODEC = simpleCodec(MagazineLoaderBlock::new);
+    public static final Property<Direction> FACING = HorizontalDirectionalBlock.FACING;
 
     public MagazineLoaderBlock(BlockBehaviour.Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
@@ -39,6 +48,27 @@ public final class MagazineLoaderBlock extends BaseEntityBlock {
     }
 
     @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected @NotNull BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    protected @NotNull BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
     protected @NotNull InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
         if (!level.isClientSide() && player instanceof ServerPlayer serverPlayer && level.getBlockEntity(pos) instanceof MagazineLoaderBlockEntity loader) {
             serverPlayer.openMenu(loader);
@@ -48,8 +78,7 @@ public final class MagazineLoaderBlock extends BaseEntityBlock {
 
     @Override
     public void destroy(LevelAccessor levelAccessor, BlockPos pos, BlockState state) {
-        if (levelAccessor instanceof Level level && level.getBlockEntity(pos) instanceof MagazineLoaderBlockEntity loader) {
-            Containers.dropContents(level, pos, loader);
+        if (levelAccessor instanceof Level level) {
             level.updateNeighbourForOutputSignal(pos, this);
         }
         super.destroy(levelAccessor, pos, state);
--- src/main/java/ttv/migami/jeg/block/entity/MagazineLoaderBlockEntity.java
+++ src/main/java/ttv/migami/jeg/block/entity/MagazineLoaderBlockEntity.java
@@ -11,6 +11,7 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.Containers;
 import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -228,6 +229,15 @@ public final class MagazineLoaderBlockEntity extends BlockEntity implements Worl
     @Override
     public void clearContent() {
         this.clearItems();
+    }
+
+    @Override
+    public void preRemoveSideEffects(BlockPos pos, BlockState state) {
+        if (this.level != null && !this.level.isClientSide()) {
+            Containers.dropContents(this.level, pos, this);
+            this.clearItems();
+        }
+        super.preRemoveSideEffects(pos, state);
     }
 
     @Override
